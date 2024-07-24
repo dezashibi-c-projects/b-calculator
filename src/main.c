61d2f2023c97e0c3ffe032d58723c4b224eacf09
Navid Dezashibi <dezashibi@gmail.com>
2024-07-24 18:53:54 +0400

    project setup

---
+++ src/main.c
@@ -0,0 +1,23 @@
+// ***************************************************************************************
+//    Project: Calculator in C
+//    File: main.c
+//    Date: 2024-07-24
+//    Author: Navid Dezashibi
+//    Contact: navid@dezashibi.com
+//    Website: https://www.dezashibi.com | https://github.com/dezashibi
+//    License:
+//     Please refer to the LICENSE file, repository or website for more information about
+//     the licensing of this work. If you have any questions or concerns,
+//     please feel free to contact me at the email address provided above.
+// ***************************************************************************************
+// *  Description: refer to readme file.
+// ***************************************************************************************
+
+#include <stdio.h>
+
+int main(void)
+{
+    puts("Welcome!");
+
+    return 0;
+}
